48072b4902b3c145c6c602750a1e7ebe58517c26
tooyama <toyama@dcl.cs.waseda.ac.jp>
2018-04-22 09:56:11 +0900

    [add] tutorial

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bullet : MonoBehaviour 
+{
+    [SerializeField]
+    float speed = 10.0f;
+
+	void Start () 
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;	
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour 
+{
+    [SerializeField]
+    float speed = 5;
+    [SerializeField]
+    float reloadTime = 0.1f;
+    [SerializeField]
+    GameObject bullet;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+            yield return new WaitForSeconds(reloadTime);
+        }
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector2 direction = new Vector2(x, y).normalized;
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
+    }
+}
